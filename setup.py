c407980f62593f77057fd037f4a1ed5de183ec92
Allison Casey <alliecasey21@gmail.com>
2019-09-17 19:36:55 -0400

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjcasey21/arma_lisp',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
