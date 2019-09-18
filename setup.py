f6f45c258656df44c57efb9488d69b7e06474389
Allison Casey <alliecasey21@gmail.com>
2019-09-17 21:16:42 -0400

    fixed readme rendering issues with pypi

--- setup.py
+++ setup.py
@@ -5,46 +5,43 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'rply', 'funcparserlib' ]
+requirements = ["Click>=7.0", "rply", "funcparserlib"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ["pytest>=3"]
 
 setup(
     author="Steve Casey",
-    author_email='stevecasey21@gmail.com',
-    python_requires='>=3.7',
+    author_email="stevecasey21@gmail.com",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
     ],
     description="A lisp dialect that compiles to sqf.",
-    entry_points={
-        'console_scripts': [
-            'arma_lisp=arma_lisp.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["arma_lisp=arma_lisp.cli:main"]},
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    long_description=readme,
     include_package_data=True,
-    keywords='arma_lisp',
-    name='arma-lisp',
-    packages=find_packages(include=['arma_lisp', 'arma_lisp.*']),
+    keywords="arma_lisp",
+    name="arma-lisp",
+    packages=find_packages(include=["arma_lisp", "arma_lisp.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/sjcasey21/arma_lisp',
-    version='0.3.4',
+    url="https://github.com/sjcasey21/arma_lisp",
+    version="0.3.4",
     zip_safe=False,
 )
