7f21349db26443ac1977272432a52d32b26e4e56
Steve Casey <stevencasey21@gmail.com>
2019-09-18 20:52:43 -0400

    adds watch option to cli. closes #3

--- arma_lisp/cli.py
+++ arma_lisp/cli.py
@@ -3,11 +3,60 @@
 """Console script for arma_lisp."""
 import sys
 import click
-import os
-import pathlib
+import time
+import logging
 
+from os import makedirs
+from pathlib import Path
 from arma_lisp import compile
 from .formatter import format
+
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+IGNORE_PATTERNS = [r".#"]
+
+
+def compile_file(pinput, poutput, file, pretty=False):
+    if any(ignore in str(file) for ignore in IGNORE_PATTERNS):
+        return None
+    makedirs(Path(poutput, file).parent, exist_ok=True)
+    with open(Path(pinput, file), "r") as fin, open(
+        Path(poutput, file.stem + ".sqf"), "w"
+    ) as fout:
+        text = fin.read()
+        text = compile(text)
+        text = format(text) if pretty else text
+        fout.write(text)
+
+
+def start_watch(input_dir, output_dir, pretty=False):
+    logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+    logging.info(f"Beginning watch in {input_dir}")
+    logging.info(f"Writing files to: {output_dir}")
+
+    def handle_compile_event(event):
+        logging.info(f"Building file: {event.src_path}")
+        compile_file(input_dir, output_dir, Path(event.src_path), pretty=pretty)
+
+    patterns = ["*.sqp"]
+    IGNORE_PATTERNS = [".#*"]
+    sqp_event_handler = PatternMatchingEventHandler(
+        patterns, IGNORE_PATTERNS, ignore_directories=True
+    )
+    sqp_event_handler.on_modified = handle_compile_event
+    sqp_event_handler.on_created = handle_compile_event
+
+    observer = Observer()
+    observer.schedule(sqp_event_handler, str(input_dir), recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
 
 @click.command()
 @click.argument("input", type=click.Path(exists=True))
@@ -20,26 +69,37 @@ from .formatter import format
 @click.option(
     "-p", "--pretty", is_flag=True, help="Pretty prints the the compiled sqf."
 )
-def main(input, output, pretty):
+@click.option(
+    "-w", "--watch", is_flag=True, help="Automatically recompile modified files."
+)
+def main(input, output, pretty=False, watch=False):
     """Console script for arma_lisp."""
 
-    path = pathlib.PurePath(input)
-    # parent = path.parent
-    # name = path.stem
+    pinput = Path(input)
+    poutput = Path(output if output else ".")
+    if pinput.is_dir():
+        if not poutput.is_dir():
+            raise ValueError("input and output paths must both be directories.")
 
-    with open(path, "r") as f:
-        sqisp_text = f.read()
+        sqp_files = pinput.rglob("*.sqp")
+        for file in sqp_files:
+            compile_file(pinput, poutput, file, pretty=pretty)
+        if watch:
+            return start_watch(pinput, poutput, pretty=pretty)
 
-    compiled_sqf = compile(sqisp_text)
+    # with open(path, "r") as f:
+    #     sqisp_text = f.read()
 
-    if pretty:
-        compiled_sqf = format(compiled_sqf)
+    # compiled_sqf = compile(sqisp_text)
 
-    if output:
-        with open(output, "w") as f:
-            f.write(compiled_sqf)
-    else:
-        click.echo(compiled_sqf)
+    # if pretty:
+    #     compiled_sqf = format(compiled_sqf)
+
+    # if output:
+    #     with open(output, "w") as f:
+    #         f.write(compiled_sqf)
+    # else:
+    #     click.echo(compiled_sqf)
     return 0
 
 
