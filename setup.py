3c91715a1d179b526f58437fefd8c1cd923b1ae3
Steve Casey <stevencasey21@gmail.com>
2019-09-20 08:34:18 -0400

    added types file to manifest

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sjcasey21/sqisp",
-    version="version='0.4.3'",
+    version="0.4.3",
     zip_safe=False,
 )
