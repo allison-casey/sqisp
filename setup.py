36c913441eb083caac3624aefb1f3b410156ce86
Allison Casey <alliecasey21@gmail.com>
2020-01-25 15:15:36 -0500

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/sjcasey21/sqisp",
-    version="0.7.2",
+    version="0.7.3",
     zip_safe=False,
 )
